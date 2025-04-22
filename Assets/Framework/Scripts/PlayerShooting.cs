0fbe9c42df4650488adb775561be62a3f81ac194
Patatsta <patrickkienzler2110@gmail.com>
2025-04-22 20:54:21 +0200

    Player

--- Assets/Framework/Scripts/PlayerShooting.cs
+++ Assets/Framework/Scripts/PlayerShooting.cs
@@ -5,37 +5,29 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private LayerMask _targetMask; // Kombiniere _enemy und _barrier im Inspector oder per Code
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-           
             RaycastHit hit;
-            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, float.PositiveInfinity))
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, float.PositiveInfinity, _targetMask))
             {
-                Debug.DrawLine(Camera.main.transform.position, hit.point, Color.red, 1f); // Nur zur Visualisierung
-               
+                Debug.DrawLine(Camera.main.transform.position, hit.point, Color.red, 1f);
 
                 if (hit.collider.CompareTag("Enemy"))
                 {
-                   
                     MonsterBehaviour monsterBehaviour = hit.collider.GetComponent<MonsterBehaviour>();
                     if (monsterBehaviour != null)
                     {
-                    
                         monsterBehaviour.Shot();
                     }
                 }
             }
         }
     }
+
 
 
     private void OnDrawGizmos()
